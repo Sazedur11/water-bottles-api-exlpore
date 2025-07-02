32ac87a396131160d0fb40b38eec0d93904138ee
Sazedur <sazedur11ab@gmail.com>
2025-07-02 23:56:24 +0600

    event handler

--- src/components/bottle/extB.jsx
+++ src/components/bottle/extB.jsx
@@ -1,16 +1,13 @@
 import './bottle.css'
 
-const Bottles = ({bottle, handleCartBottle}) => {
-
+const Bottles = ({bottle}) => {
     console.log(bottle)
     const {img, name, price} = bottle;
-
     return (
-        <div className="bottle">
+        <div>
             <img className="image" src={img} alt="" />
             <h3>Name: {name}</h3>
             <p>Price: {price}</p>
-            <button onClick={() => handleCartBottle(bottle)}>Buy Now</button>
         </div>
     );
 };
--- src/components/bottles/extra.jsx
+++ src/components/bottles/extra.jsx
@@ -1,3 +1,4 @@
+// bottles components
 import Bottle from "../bottle/Bottle";
 import './Bottles.css'
 import { useEffect, useState } from "react";
@@ -5,7 +6,6 @@ import { useEffect, useState } from "react";
 const Bottles = () => {
 
     const [bottles, setBottles] = useState([]);
-    const [cart, setCart] = useState([])
 
     useEffect(() => {
         fetch('bottles.json')
@@ -13,22 +13,12 @@ const Bottles = () => {
         .then(data => setBottles(data))
     },[])
 
-    const handleCartBottle = bottle =>{
-        const newCart = [...cart, bottle];
-        setCart(newCart)
-    }
-
     return (
         <div>
-            <h3>Available Bottles: {bottles.length}</h3>
-            <h4>Cart: {cart.length}</h4>
+            <h3>Bottles: {bottles.length}</h3>
             <div className="bottles">
                 {
-                    bottles.map(bottle => <Bottle 
-                        key={bottle.id} 
-                        bottle={bottle}
-                        handleCartBottle={handleCartBottle}
-                        ></Bottle>)
+                    bottles.map(bottle => <Bottle key={bottle.id} bottle={bottle}></Bottle>)
                 }
             </div>
         </div>
